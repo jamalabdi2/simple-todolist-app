5d51aa99abdc6678ef24dfb8e85660e409559040
jamal <abdijumale2@gmail.com>
2023-06-10 20:29:28 +0900

    last commit

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,9 @@ from .import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('create/',views.create_task,name = 'create_task'),
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name ='mark_as_done'),
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name ='mark_as_undone'),
+    path('edit_task/<int:pk>/',views.edit_task,name ='edit_task'),
+    path('delete_task/<int:pk>/',views.delete_task,name ='delete_task'),
     
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
 from django.http import HttpResponseRedirect
 
+
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
-    context = {'tasks':tasks}
+    completed_tasks = Task.objects.filter(is_completed = True)
+    context = {
+        'tasks':tasks,
+        'completed_tasks':completed_tasks
+        }
 
     print(tasks)
+    print(completed_tasks)
   
     return render(request,'home-todo1.html',context)
 
@@ -16,7 +22,32 @@ def create_task(request):
     print(request.POST['task'])
     return redirect('home')
 
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
 
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
 
+def edit_task(request,pk):
+    get_task = get_object_or_404(Task,id=pk) 
+    if request.method =='POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        print(new_task)
+        return redirect('home')
+    context = {'get_task':get_task}
+    return render(request,'todos/edit_task.html',context)
+
+def delete_task(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
 
 
